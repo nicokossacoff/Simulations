4e15caa32c1ce3b68088ae845c8bb8de599e4552
Nicolás Kossacoff <nicokossacoff@gmail.com>
2024-06-04 16:21:55 -0300

    Minor changes

--- Dev/figures.py
+++ Dev/figures.py
@@ -9,34 +9,40 @@ warnings.filterwarnings("ignore")
 ## This function is used to generate 1K random samples with n observations each
 ## We calculate the mean sample for each random sample and then we plot its ditribution
 
-def mean_sample_distribution(sample_size: int, probability: float = 0.35):
+def mean_sample_distribution(sample_size: int, probability: float):
     estimations = np.zeros(shape= 1000)
     for i in np.arange(start= 0, stop= 1000, step= 1):
         sample = np.random.binomial(n= 1, p= probability, size= sample_size)
         estimations[i] = np.mean(sample)
     
-    hist = go.Histogram(histfunc= "count",
-                        x= estimations,
-                        nbinsx= 15,
-                        marker= dict(color= "#4DBAC1",
-                                     line= dict(width= 1, color= "black")
-                                    )
-                        )
+    hist = go.Histogram(
+        histfunc= "count",
+        x= estimations,
+        nbinsx= 15,
+        marker= dict(color= "#4DBAC1", 
+                     line= dict(width= 1, color= "black")
+                     )
+    )
+
     fig = go.Figure(data= [hist])
-    fig.update_layout(yaxis= dict(title= "Frequency",
-                                  showline= True,
-                                  linecolor= "black",
-                                  ticks= "outside",
-                                  gridcolor= "lightgrey"),
-                      xaxis= dict(showline= True,
-                                  linecolor= "black",
-                                  ticks= "outside",
-                                  gridcolor= "lightgrey",
-                                  range= [0, 1]),
-                      plot_bgcolor= "white",
-                      title= dict(text= f"Sample Mean distribution with n = {sample_size}",
-                                  font= dict(size= 16, color= "black", family= "Arial Black")
-                                  ),
-                      height= 800  
-                      )          
+    fig.update_layout(
+        yaxis= dict(title= "Frequency",
+                    showline= True,
+                    linecolor= "black",
+                    ticks= "outside",
+                    gridcolor= "lightgrey"
+                    ),
+        xaxis= dict(showline= True,
+                    linecolor= "black",
+                    ticks= "outside",
+                    gridcolor= "lightgrey",
+                    range= [0, 1]
+                    ),
+        plot_bgcolor= "white",
+        title= dict(text= f"Sample Mean distribution with n = {sample_size} and p = {probability}",
+                    font= dict(size= 16, color= "black", family= "Arial Black")
+                    ),
+        height= 800  
+    )
+             
     return fig
